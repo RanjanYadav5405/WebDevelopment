4ce9ca2ab6b70d7774c57197bf25ef1f0eaf262b
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-07-16 18:47:22 +0530

    JavaScript Variables, Data Types & Objects

---
+++ 54_Introduction to JavaScript & Installing Node.js/script.js
@@ -0,0 +1,18 @@
+alert("Hello world")
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like wow..")
+
+var a = prompt("Enter Your number:")
+console.log(a)
+
+let isTrue = confirm("your computer is blasting if you comfirm")
+if (isTrue) {
+    console.log("your computer is blasting")
+}
+else {
+    console.log("your computer is not blasting")
+}
+
+document.title = "I am Ranjan";
+// document.body.style.backgroundColor="red"
---
+++ 55_JavaScript Variables, Data Types & Objects/script.js
@@ -0,0 +1,41 @@
+console.log("Hey this is tutorial 55");
+
+var a = 5;
+let b  = 6;
+let c = "Harry"
+// let _a = "Shubham"
+// var 55a ="Rohan"; //Not Allowed
+
+// console.log(a + b + 8)
+// console.log(typeof a + typeof b + typeof c)
+ 
+{
+    // let a =66;
+    console.log(a)
+}
+console.log(a)
+
+// const a1 = 6;
+// a1 = a1 + 1;  // Not allowed beacuse a1 is constant
+// console.log(a1)
+
+let x = "Ranjan Yadav";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+}
+console.log(o)
+o.salary ="100cr";
+console.log(o)
+o.salary = "500cr"
+console.log(o)
