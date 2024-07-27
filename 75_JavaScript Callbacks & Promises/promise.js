37691befdbdb614663fb5c7cc4292cdafb7aba50
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-07-27 22:14:34 +0530

    callback function and promise

---
+++ 75_JavaScript Callbacks & Promises/promise.js
@@ -0,0 +1,76 @@
+console.log("This is Promise");
+
+
+let prom1 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you 1")
+    }
+  else{
+        setTimeout(() => {
+            console.log('Yes I am done 1' );
+            resolve("Ranjan 1")
+        }, 3000);
+    }
+})
+
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you 2")
+    }
+  else{
+        setTimeout(() => {
+            console.log('Yes I am done 2');
+            resolve("Ranjan 2")
+        }, 3000);
+    }
+})
+
+let prom3 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you 3")
+    }
+  else{
+        setTimeout(() => {
+            console.log('Yes I am done 3');
+            resolve("Ranjan 3")
+        }, 3000);
+    }
+})
+
+let prom4 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you 4")
+    }
+  else{
+        setTimeout(() => {
+            console.log('Yes I am done 4');
+            resolve("Ranjan 4")
+        }, 3000);
+    }                 
+})
+
+let prom5 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a < 0.5) {
+        reject("No random number was not supporting you 5")
+    }
+  else{
+        setTimeout(() => {
+            console.log('Yes I am done 5');
+            resolve("Ranjan 5")
+        }, 3000);
+    }
+})
+
+const p5 = Promise.resolve([prom1, prom2, prom3, prom4, prom5])
+p5.then((a) => {
+    console.log(a);
+    
+}).catch((err) => {
+  console.log(err);
+  
+})
---
+++ 75_JavaScript Callbacks & Promises/script.js
@@ -0,0 +1,40 @@
+console.log('Ranjan is Hacker');
+console.log('Yadav is hecker');
+
+setTimeout(() => {
+    console.log('I am inside settimeout');
+    
+}, 3000);
+
+setTimeout(() => {
+    console.log('I am inside settimeout 2');
+    
+}, 0);
+
+console.log('The End');
+
+
+//===========================================================
+
+// call function
+
+const fn =() =>{
+    console.log('Nothing');
+    
+}
+
+const callback = (arg, fn) =>{
+    console.log(arg);
+    fn();
+    
+}
+
+
+const loadScript = (src , callback) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+    sc.onload = callback("RanjanYadav",fn);
+    document.head.append(sc);
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback)
