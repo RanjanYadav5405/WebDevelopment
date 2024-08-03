10c9b843405d17262916f88a034b2b011624ff25
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-08-03 16:44:07 +0530

    CommonJs an EcmaScript Modules in NodeJs

---
+++ 86_CommonJs Vs EcmaScript Modules/main.js
@@ -0,0 +1,41 @@
+// CommonJs using require
+
+// const { createServer } = require('node:http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// }); 
+
+//=====================================================
+
+// EcmaScript Modules using module exports are two type one is named and other is default
+
+// import {a, b, c, d} from "./mymodule.js"
+// console.log(a,b,c,d);
+
+// import ranjan from "./mymodule.js"
+// console.log(ranjan)
+
+
+//=====================================================================
+
+// convert ecmaJs export to using require
+
+const a = require("./mymodule2")
+
+console.log(a, __dirname, __filename)
+
+
+// require dirname and filename coming from this function but we can't see nodeJs automatically wrap
+
+// (function(exports, require, module, __filename, __dirname) {
+
+//     // Module code actually lives here
+  
+//   });
---
+++ 86_CommonJs Vs EcmaScript Modules/mymodule2.js
@@ -0,0 +1,8 @@
+module.exports = {
+     a: 1, 
+     b: 4
+
+}
+
+let c = 56;
+module.exports = c
